f5caadd12a21b54111f584142aff980e0c50f87b
Kishore Hebbar <hebbar.kishore@gmail.com>
2020-09-20 22:10:20 +0000

    Test

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const largeARr = new Array(100).fill('nemo');
 function justCheck(array) {
   let t1 = performance.now();
   array.forEach( s => console.log('value:'+s));
-}
+} 
 
 justCheck(largeARr);//Adding the changes
