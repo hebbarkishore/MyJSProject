59ee15ba2c5f6dac59d854874f064628863aa2a7
Kishore Hebbar <hebbar.kishore@gmail.com>
2020-09-20 21:47:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('my name is ksh');
+const {performance} = require('perf_hooks');
+//var module = require('ES6')
+
+//console.log('milliseconds:'+t1)
+const largeARr = new Array(100).fill('nemo');
+function justCheck(array) {
+  let t1 = performance.now();
+  array.forEach( s => console.log('value:'+s));
+}
+
+justCheck(largeARr);
